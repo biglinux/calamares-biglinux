5924004f7ee061ce68463e10c135af8e0d36fdb6
Bruno Goncalves <bigbruno@gmail.com>
2021-07-21 17:58:29 -0300

    ok

--- usr/bin/grub-fix.sh
+++ usr/bin/grub-fix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\"|GRUB_CMDLINE_LINUX_DEFAULT=\"$(cat /proc/cmdline | sed 's|toram||g;s|splash||g;s|.*driver=free||;s|.*driver=nonfree||g;s|misobasedir=manjaro ||g;s|misolabel=M1804 ||g')|g" $*
+#sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\"|GRUB_CMDLINE_LINUX_DEFAULT=\"$(cat /proc/cmdline | sed 's|toram||g;s|splash||g;s|.*driver=free||;s|.*driver=nonfree||g;s|misobasedir=manjaro ||g;s|misolabel=M1804 ||g')|g" $*
 
 
 
 #sed -i "s|security=apparmor ||g;s|apparmor1 ||g;s|udev.log_priority=3||g;s|quiet| quiet splash |g')|g" $*
 
-sed -i 's|GRUB_THEME=.*|GRUB_THEME=/boot/grub/themes/biglinux/theme.txt|g' $*
+sed -i 's|GRUB_THEME=.*|GRUB_THEME="/boot/grub/themes/biglinux/theme.txt"|g' $*
 
 sed -i 's|GRUB_SAVEDEFAULT=true|GRUB_SAVEDEFAULT=false|g' $*
 
