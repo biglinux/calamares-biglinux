749a45c098ded9f3b721b81fcf6592dae81ef85b
Bruno Gonçalves <bigbruno@gmail.com>
2023-10-12 19:04:43 -0300

    Testing zstd value

--- usr/bin/grub-fix.sh
+++ usr/bin/grub-fix.sh
@@ -5,7 +5,7 @@
 
 sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\"|GRUB_CMDLINE_LINUX_DEFAULT=\"$(sed 's|BOOT_IMAGE=/boot/vmlinuz-x86_64 ||g;s| driver=nonfree||g;s| driver=free||g;s| rdinit=/vtoy/vtoy||g;s| quiet splash||g' /proc/cmdline) |g" $*
 
-sed -i 's|BOOT_IMAGE=/boot/vmlinuz-x86_64||g;s|misobasedir=manjaro misolabel=biglinux ||g' $*
+sed -i 's|BOOT_IMAGE=/boot/vmlinuz-x86_64||g;s|misobasedir=manjaro misolabel=BIGLINUXLIVE ||g' $*
 
 sed -i 's|GRUB_THEME=.*|GRUB_THEME="/boot/grub/themes/biglinux/theme.txt"|g' $*
 
@@ -13,28 +13,24 @@ sed -i 's|GRUB_SAVEDEFAULT=true|GRUB_SAVEDEFAULT=false|g;s|quiet quiet|quiet|g' 
 
 #remove multiples splash
 while [ "$(grep -o '[^[:space:]]*splash[^[:space:]]*' $* | sed 's/\"//' | wc -w)" -gt "1" ];do
-    sed -i 's/ splash//' $*
+    sed -i 's/ splash//' $*kk
 done
 #remove multiples quiet
 while [ "$(grep -o '[^[:space:]]*quiet[^[:space:]]*' $* | sed 's/\"//' | wc -w)" -gt "1" ];do
     sed -i 's/ quiet//' $*
 done
 
-
-#Change default desktop in sddm to use in livecd
-echo "[Last]
-Session=/usr/share/xsessions/plasma-biglinux-x11.desktop" > $(echo "$*" | sed 's|etc/default/grub|var/lib/sddm/state.conf|g')
-
-sed -i "s|Session=plasma.desktop|Session=plasma-biglinux-x11.desktop|g" $(echo "$*" | sed 's|etc/default/grub|etc/sddm.conf|g')
+# use compress-force=zstd:5 in /git 
+inux-x11.desktop|g" $(echo "$*" | sed 's|etc/default/grub|etc/sddm.conf|g')
 
 
 # Save default KDE configuration
 
 cp -f "/etc/big_desktop_changed" $(echo "$*" | sed 's|etc/default/grub|/etc/big_desktop_changed|g')
 
-if [ -e "/tmp/use_disable_fsync" ]; then
-    echo '/usr/$LIB/disable-fsync.so' > $(echo "$*" | sed 's|etc/default/grub|etc/ld.so.preload|g')
-fi
+# if [ -e "/tmp/use_disable_fsync" ]; then
+#     echo '/usr/$LIB/disable-fsync.so' > $(echo "$*" | sed 's|etc/default/grub|etc/ld.so.preload|g')
+# fi
 
 
 cp -f /tmp/big_desktop_theme $(echo "$*" | sed 's|etc/default/grub|etc/default-theme-biglinux|g')
